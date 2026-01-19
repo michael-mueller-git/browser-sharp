c5bd292d68e2f8da366ea1d2fa76e8648aa6f30d
nix <nix@local>
2026-01-19 18:21:43 +0100

    add deployment stuff

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "browser-sharp built with Nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.default = pkgs.buildNpmPackage {
+          pname = "browser-sharp";
+          version = "0.1.0";
+          src = ./.;
+          # npmDepsHash = pkgs.lib.fakeHash;
+          npmDepsHash = "sha256-Ql7dv8xfc+dbIKz6FV4VTjE41a1R1L392Au/flGLMpw=";
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out
+            cp -r dist/* $out/
+            runHook postInstall
+          '';
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_20
+            nodePackages.npm
+          ];
+        };
+
+        apps.default = {
+          type = "app";
+          program = toString (pkgs.writeShellScript "serve-browser-sharp" ''
+            set -eu
+            ${pkgs.python3}/bin/python3 -m http.server 4173 -d ${self.packages.${system}.default}
+          '');
+        };
+      }
+    );
+}
